7c0fe3bc014f821969de4638b6ac83317b208877
yann <tmp4321@qq.com>
2019-03-26 01:15:17 +0800

    chitchat static file

---
+++ chitchat/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+
+	// index
+	mux.HandleFunc("/", index)
+
+	// starting up the server
+	server := &http.Server{
+		Addr:           "0.0.0.0:8080",
+		Handler:        mux,
+	}
+	server.ListenAndServe()
+}
