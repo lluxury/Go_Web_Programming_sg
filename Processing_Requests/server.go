10306b54baa32386774affc1b083d57b8e21efad
yann <tmp4321@qq.com>
2019-04-17 17:45:18 +0800

    ParseMultipartForm

--- Processing_Requests/server.go
+++ Processing_Requests/server.go
@@ -5,21 +5,17 @@ import (
 	"net/http"
 )
 
-// func body(w http.ResponseWriter, r *http.Request)  {
 func process(w http.ResponseWriter, r *http.Request)  {
-	r.ParseForm()
+	// r.ParseForm()
+	r.ParseMultipartForm(1024)
 	// fmt.Fprintln(w,r.Form)
-	// fmt.Fprintln(w,r.Form["hello"])
-	// fmt.Fprintln(w,r.PostForm)
-	fmt.Fprintln(w,r.Form)
+	fmt.Fprintln(w,r.MultipartForm)
 }
-// 使用 ParseForm对请求进行语法分析,再访问Form字段,获取表单
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	// http.HandleFunc("/body", body)
 	http.HandleFunc("/process", process)
 	server.ListenAndServe()
 }
