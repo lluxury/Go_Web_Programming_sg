9d2f5a0ff20650856fbb67327d75cf2c0d59a61e
yann <tmp4321@qq.com>
2019-04-17 18:13:13 +0800

    PostFormValue

--- Processing_Requests/server.go
+++ Processing_Requests/server.go
@@ -6,10 +6,10 @@ import (
 )
 
 func process(w http.ResponseWriter, r *http.Request)  {
-	r.ParseMultipartForm(1024)
-	// fmt.Fprintln(w,r.MultipartForm)
-	fmt.Fprintln(w,r.FormValue("hello"))
-	fmt.Fprintln(w,r.Form)
+	// r.ParseMultipartForm(1024)
+	// r.ParseForm
+	fmt.Fprintln(w,r.PostFormValue("hello"))
+	fmt.Fprintln(w,r.PostForm)
 }
 
 func main() {
