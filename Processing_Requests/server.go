11f89a99c0e6c811d2d519479b7e45aba6e5082a
yann <tmp4321@qq.com>
2019-04-18 15:46:34 +0800

    ResponseWriter.Header

--- Processing_Requests/server.go
+++ Processing_Requests/server.go
@@ -19,11 +19,18 @@ func writeHeaderExample(w http.ResponseWriter , r *http.Request)  {
 	fmt.Fprintln(w, "No such service, try next door")
 }
 
+func headerExample(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+	
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/write", writeExample)
 	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
 	server.ListenAndServe()
 }
