4ad22d6f755e613ce7e752309c82784b72659269
yann <tmp4321@qq.com>
2019-04-18 15:30:55 +0800

    ResponseWriter.WriteHeader

--- Processing_Requests/server.go
+++ Processing_Requests/server.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"net/http"
 	// "io/ioutil"
 )
 
-// func process(w http.ResponseWriter, r *http.Request)  {
 func writeExample(w http.ResponseWriter, r *http.Request)  {
 	str := `<html>
 <head><title> Go Web Programming</title></head>
@@ -15,11 +14,16 @@ func writeExample(w http.ResponseWriter, r *http.Request)  {
 	w.Write([]byte(str))
 }
 
+func writeHeaderExample(w http.ResponseWriter , r *http.Request)  {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "No such service, try next door")
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	// http.HandleFunc("/process", process)
 	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
 	server.ListenAndServe()
 }
