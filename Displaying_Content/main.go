0e6af82dc54e3d37e0ad30e8f7c0e83f9f2f6ae5
yann <tmp4321@qq.com>
2019-04-25 12:00:24 +0800

    custom_function

--- Displaying_Content/main.go
+++ Displaying_Content/main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"time"
 )
 
+func formatDate(t time.Time) string  {
+	layout := "2019-04-25"
+	return t.Format(layout)
+}
+
 func process(w http.ResponseWriter, r *http.Request) {
-	// t, _ := template.ParseFiles("tmp1.html")
-	t, _ := template.ParseFiles("t1.html", "t2.html")
-	t.Execute(w, "Hello World!")
+
+	// funcMap := template.FuncMap("fdate": formatDate)
+	funcMap := template.FuncMap{"fdate": formatDate}
+	
+	t :=template.New("tmp1.html").Funcs(funcMap)
+	// t, _ := template.ParseFiles("t1.html", "t2.html")
+	
+	t, _ = t.ParseFiles("tmp1.html")
+	// t.Execute(w, "Hello World!")
+	
+	t.Execute(w, time.Now())
 }
 
 func main() {
