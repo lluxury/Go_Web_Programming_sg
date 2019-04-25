a24e91499d76d24dbccac5c305c59570fc840b76
yann <tmp4321@qq.com>
2019-04-25 15:26:02 +0800

    use many template

--- Displaying_Content/main.go
+++ Displaying_Content/main.go
@@ -3,22 +3,20 @@ package main
 import (
 	"html/template"
 	"net/http"
+	// "rand"
+	"math/rand"
+	"time"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	// t, _ := template.ParseFiles("tmp1.html")
-	t, _ := template.ParseFiles("layout.html")
-	
-	// t.Execute(w, r.FormValue("comment"))
+	rand.Seed(time.Now().Unix())
+	var t *template.Template
+	if rand.Intn(10) > 5 {
+		t, _ = template.ParseFiles("layout.html", "red_hello.html")
+	} else {
+		t, _ = template.ParseFiles("layout.html", "blue_hello.html")
+	}
 	t.ExecuteTemplate(w, "layout", "")
-}
-
-func form(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("form.html")
-	// t.Execute(w, nil)
-	w.Header().Set("X-XSS-Protection", "0")
-	t.Execute(w, template.HTML(r.FormValue("comment")))
-
 }
 
 func main() {
@@ -26,7 +24,6 @@ func main() {
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/process", process)
-	http.HandleFunc("/form", form)
 
 	server.ListenAndServe()
 }
