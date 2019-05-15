0a56de90ea8364545a37ebac60ac58b21b2cb688
yann <tmp4321@qq.com>
2019-05-15 18:00:22 +0800

    sql gorm

--- Storing_Data/store.go
+++ Storing_Data/store.go
@@ -2,49 +2,59 @@ package main
 
 import (
 	"fmt"
-	"github.com/jmoiron/sqlx"
+	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 type Post struct {
 	Id int
 	Content string
-	// Author  string
-	AuthorName string `db:author`
-
+	Author  string `sql:"not null"`
+	// AuthorName string `db:author`
+	Comments []Comment
+	CreatedAT time.Time
 }
 
-// var Db *sql.DB
-var Db *sqlx.DB
+type Comment struct {
+	Id int
+	Content string
+	Author  string `sql:"not null"`
+	PostId int  `sql:"index"`
+    CreatedAT time.Time
+}
+
+// var Db gorm.DB
+var Db *gorm.DB
 
 func init()  {
 	var err error
-	// Db, err = sql.Open("postgres", "postgres://gwp:gwp@192.168.142.9/gwp?sslmode=disable")
-	Db, err = sqlx.Open("postgres", "postgres://gwp:gwp@192.168.142.9/gwp?sslmode=disable")
+	// Db, err = sqlx.Open("postgres", "postgres://gwp:gwp@192.168.142.9/gwp?sslmode=disable")
+	Db, err = gorm.Open("postgres", "postgres://gwp:gwp@192.168.142.9/gwp?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}	// 连接数据库
+	// Db.AuthoMigrate(&Post{}, &Comment{})
+	Db.AutoMigrate(&Post{}, &Comment{})
 }
-
-func GetPost(id int) (post Post, err error) {
-	post = Post{}
-	// err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
-	err = Db.QueryRowx("select id, content, author from posts where id = $1", id).StructScan(&post)
-	if err != nil {
-		return
-	}
-	return
-}	
-// func (post *Post) Create() (err erros){
-func (post *Post) Create() (err error){
-	err = Db.QueryRow("insert into posts (content, author) values ($1, $2) returning id", post.Content, post.AuthorName).Scan(&post.Id)
-	return
-}
-
 
 func main() {
-	post := Post{Content: "Hello World!", AuthorName: "Sau Sheong"}
-	post.Create()
+	// post := Post{Content: "Hello World!", AuthorName: "Sau Sheong"}
+	post := Post{Content: "Hello World!", Author: "Sau Sheong"}
+	fmt.Println(post)
+	// {0 Hello World! Sau Sheong [] 0001-01-01 00:00:00 +0000 UTC}
 
-	fmt.Println(post)                  // {1 Hello World! Sau Sheong [{1 Good post! Joe 0xc20802a1c0}]}
+	Db.Create(&post)  // 创建帖子
+	fmt.Println(post)
+	// {5 Hello World! Sau Sheong [] 2019-05-15 17:41:50.1946001 +0800 CST m=+0.059000001}
+	
+	comment := Comment{Content: "Good post!", Author: "Joe"}	// 添加评论
+	Db.Model(&post).Association("Comments").Append(comment)
+	
+	var readPost Post
+	Db.Where("author = $1", "Sau Sheong").First(&readPost)  // 通过帖子获取评论
+	  var comments []Comment
+	  Db.Model(&readPost).Related(&comments)
+	fmt.Println(comments[0])  
+	// {1 Good post! Joe 1 0001-01-01 00:00:00 +0000 UTC}
 }
