e8e179180564b3ad856b251f43d38af2697175ad
yann <tmp4321@qq.com>
2019-05-23 18:07:24 +0800

    MarshalIndent

--- Creating_Web_Services/xml.go
+++ Creating_Web_Services/xml.go
@@ -38,7 +38,8 @@ func main() {
 		},
 	}
 
-	output, err := xml.Marshal(&post)
+	// output, err := xml.Marshal(&post)
+	output, err := xml.MarshalIndent(&post, "","\t")
 	if err != nil {
 		fmt.Println("Error marshalling to XML :", err)
 		return
